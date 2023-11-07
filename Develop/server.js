996ab699f291d863ea19d6b6448555c81e84b436
Graham Marshall <gmarshalltech1@gmail.com>
2023-11-06 21:08:05 -0700

    Created back end but for got to do multiple commits

---
+++ Develop/server.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const path = require("path")
+const fs = require("fs")
+
+const app = express();
+
+// middleware
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+
+// app.get("/assets/css/styles.css", (req, res) => {
+//     res.sendFile(path.join(__dirname, "./public/assets/css/styles.css"))
+// })
+
+
+// html routes
+app.get("/notes", (req, res) => {
+    // res.send("Hi!")
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
+})
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+})
+
+// api routes
+app.get("/api/notes", (req, res) => {
+    fs.readFile("./db/db.json", "utf-8", (err, content) => {
+        if(err) {
+            console.log(err);
+        }
+
+        const data = JSON.parse(content)
+
+        res.json(data)
+    })
+})
+
+app.post("/api/notes", (req, res) => { 
+
+    console.log(req.body);
+
+    fs.readFile("./db/db.json", "utf-8", (err, content) => {
+        if(err) {
+            console.log(err);
+        }
+
+        const data = JSON.parse(content);
+
+        data.push({
+            title: req.body.title,
+            text: req.body.text,
+        })
+
+        fs.writeFile("./db/db.json", JSON.stringify(data, null, 4), (err) => {
+
+            res.json(data)
+        })
+
+    })
+})
+
+
+
+
+
+app.listen(3001, () => {
+    console.log("Server is running")
+})
